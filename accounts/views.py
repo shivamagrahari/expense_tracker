4d4acf2873829af5a63872bcc0c73356f0b85f6c
Shivam Agrahari (shiagrah) <shiagrah@cisco.com>
2019-04-10 18:04:10 +0530

    login and signup module implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+import requests
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
